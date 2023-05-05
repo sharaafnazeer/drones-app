5f3a57d79576e206df31d64210f5a4a196fb6077
Sharaaf Nazeer <ahamedsharaaf@gmail.com>
2023-05-05 17:02:23 +0530

    Initial commit with spring boot installation

---
+++ src/main/java/com/sharaafnazeer/dronesapp/DronesAppApplication.java
@@ -0,0 +1,13 @@
+package com.sharaafnazeer.dronesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharaafnazeer/dronesapp/DronesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharaafnazeer.dronesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
